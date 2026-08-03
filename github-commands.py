4c342092190d5551c9ba84067eeb206fb0e8aa8b
Tejas <tejaskhare360@gmail.com>
2026-08-03 13:32:23 +0530

    Add files via upload

---
+++ github-commands.py
@@ -0,0 +1,11 @@
+#Create a new local repository
+
+$ git init [project name]
+$ git clone git_url
+$ git clone git_url my_directory
+
+#Show modified files in working directory, staged for your next commit
+
+$ git status
+
+
