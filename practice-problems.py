3142c5c1e1eab63fd7fc3b0c02fd1e96ad63190b
Tejas <tejaskhare360@gmail.com>
2026-08-03 13:32:58 +0530

    Add files via upload

---
+++ practice-problems.py
@@ -0,0 +1,10 @@
+#WAP to input user's first name and print it's length. 
+
+name = input("enter your first name:") 
+print(len(name))
+
+#wap to find the occuracne of '$' in a string. 
+
+acc= "$$$ making day and night" 
+print(acc.count("$"))
+
